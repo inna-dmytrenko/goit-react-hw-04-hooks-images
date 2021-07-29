a7c4f7fbb63a23fc7b255859d827ef26fab0c60c
inna-dmytrenko <73888831+inna-dmytrenko@users.noreply.github.com>
2021-07-29 23:15:47 +0300

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,87 +1,83 @@
-import 'react-toastify/dist/ReactToastify.css'
-import { useState, useEffect } from 'react'
-import { toast } from 'react-toastify'
-import Searchbar from './Searchbar/Searchbar'
-import ImageGallery from './ImageGallery/ImageGallery'
-import Button from './Button/Button'
-import Modal from './Modal/Modal'
-import Loader from './Loader/Loader'
-import { ToastContainer } from 'react-toastify'
-import { AppStyled } from './App.styled'
-import getPixabay from './services/pixabay-api'
+import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+import Loader from './Loader/Loader';
+import { ToastContainer } from 'react-toastify';
+import { AppStyled } from './App.styled';
+import getPixabay from './services/pixabay-api';
 
 export default function App() {
-  const [query, setQuery] = useState('')
-  const [page, setPage] = useState(1)
-  const [images, setImage] = useState([])
-  const [modal, setModal] = useState('')
-  const [loader, setLoader] = useState(false)
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImage] = useState([]);
+  const [modal, setModal] = useState('');
+  const [loader, setLoader] = useState(false);
 
-  const closeModalByEsc = (e) => {
+  const closeModalByEsc = e => {
     if (e.key === 'Escape') {
-      setModal('')
+      setModal('');
     }
-  }
+  };
   useEffect(() => {
     if (modal) {
-      window.addEventListener('keyup', closeModalByEsc)
+      window.addEventListener('keyup', closeModalByEsc);
     } else {
-      window.removeEventListener('keyup', closeModalByEsc)
+      window.removeEventListener('keyup', closeModalByEsc);
     }
-  }, [modal])
-  const handleSetQuery = (e) => {
-    setQuery(e.target.value)
-  }
+  }, [modal]);
+  const handleSetQuery = e => {
+    setQuery(e.target.value);
+  };
 
-  const handleGetImages = async (e) => {
-    e.preventDefault()
+  const handleGetImages = async e => {
+    e.preventDefault();
 
-    setPage(1)
-    setLoader(true)
+    setPage(1);
+    setLoader(true);
     if (query.trim() === '') {
-      setLoader(false)
-      setQuery('')
-      return toast.warning('Please enter something!')
+      setLoader(false);
+      setQuery('');
+      return toast.warning('Please enter something!');
     } else {
-      setLoader(false)
+      setLoader(false);
 
-      const {
-        data: { hits },
-      } = await getPixabay(query, 1)
+      const { hits } = await getPixabay(query, 1);
 
-      setLoader(false)
-      setImage(hits)
+      setLoader(false);
+      setImage(hits);
 
       if (hits.length < 1) {
-        setQuery('')
+        setQuery('');
         return toast.warning(
           `Your search - ${query} - did not match any images`,
-        )
+        );
       }
 
-      setPage((prev) => prev + 1)
+      setPage(prev => prev + 1);
     }
-  }
+  };
   const handleLoadMore = async () => {
-    const {
-      data: { hits },
-    } = await getPixabay(query, page)
-    setImage((prev) => [...prev, ...hits])
-    setPage((prev) => prev + 1)
+    const { hits } = await getPixabay(query, page);
+    setImage(prev => [...prev, ...hits]);
+    setPage(prev => prev + 1);
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       left: 0,
       behavior: 'smooth',
-    })
-  }
-  const handleShowModal = (url) => {
-    setModal(url)
-  }
-  const handleCloseModal = (e) => {
+    });
+  };
+  const handleShowModal = url => {
+    setModal(url);
+  };
+  const handleCloseModal = e => {
     if (e.target.nodeName !== 'IMG') {
-      setModal('')
+      setModal('');
     }
-  }
+  };
 
   return (
     <AppStyled>
@@ -98,5 +94,5 @@ export default function App() {
 
       <ToastContainer />
     </AppStyled>
-  )
+  );
 }
--- src/components/services/pixabay-api.js
+++ src/components/services/pixabay-api.js
@@ -1,13 +1,23 @@
-import axios from 'axios'
+// import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/'
-const API_KEY = '21764210-8d882ab68fe5176a0369b7247'
-function getPixabay(query, page) {
-  
-  return axios({
-    method: 'GET',
-    url: `${BASE_URL}?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&key=${API_KEY}`,
-  })
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '21764210-8d882ab68fe5176a0369b7247';
+
+// function getPixabay(query, page) {
+//   return axios({
+//     method: 'GET',
+//     url: `${BASE_URL}?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&key=${API_KEY}`,
+//   });
+// }
+async function fetchWithErrorHandling(url = '', config = {}) {
+  const response = await fetch(url, config);
+  return response.ok
+    ? await response.json()
+    : Promise.reject(new Error('Not found'));
 }
-
-export default getPixabay
+export function getPixabay(query, page) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&key=${API_KEY}`,
+  );
+}
+export default getPixabay;
